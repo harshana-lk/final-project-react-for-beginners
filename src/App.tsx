d6278a4ba5f933660c457098c9115a2ff7f384d5
harshana-lk <harshuonlive@gmail.com>
2024-12-19 22:48:41 +0530

    Responsive layout design

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+
+function App() {
+  // useBreakpointValue
+  const showSidebar = useBreakpointValue({ base: false, lg: true });
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" "footer"`,
+        lg: `"nav nav" "aside main" "footer footer"`,
+      }}
+    >
+      <GridItem area="nav" bg={"blue"}>
+        NavBar
+      </GridItem>
+
+      {/* Using useBreakpointValue */}
+      {showSidebar && (
+        <GridItem area="aside" bg={"red"}>
+          Side Bar
+        </GridItem>
+      )}
+      <GridItem area="main" bg={"green"}>
+        Main Content
+      </GridItem>
+      <GridItem area="footer" bg={"brown"}>
+        Footer
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
